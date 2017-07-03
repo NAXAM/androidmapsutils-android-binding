f4a5d1b07b15b8c3040c29da8d057a4f23f73b7b
Vu Duc Tuyen <tuyen@naxam.net>
2017-07-03 17:13:11 +0700

    Android Maps Utils 0.4.1

---
+++ Naxam.AndroidMapsUtils.Droid/Additions/Classes.cs
@@ -0,0 +1,45 @@
+﻿using Droid = Android;
+
+namespace Com.Google.Maps.Android.Kml
+{
+    partial class KmlMultiGeometry
+    {
+        public Java.Lang.Object GeometryObject
+        {
+            get
+            {
+                return new Droid.Runtime.JavaList<Com.Google.Maps.Android.Kml.IKmlGeometry>(KmlGeometryObject);
+            }
+        }
+	}
+	partial class KmlLineString
+	{
+		public Java.Lang.Object GeometryObject
+		{
+			get
+			{
+                return new Droid.Runtime.JavaList<Droid.Gms.Maps.Model.LatLng>(KmlGeometryObject);
+			}
+		}
+	}
+	partial class KmlPoint
+	{
+		public Java.Lang.Object GeometryObject
+		{
+			get
+			{
+				return KmlGeometryObject;
+			}
+		}
+	}
+	partial class KmlPolygon
+	{
+		public Java.Lang.Object GeometryObject
+		{
+			get
+			{
+                return new Droid.Runtime.JavaList<System.Collections.Generic.IList<Droid.Gms.Maps.Model.LatLng>>(KmlGeometryObject);
+			}
+		}
+	}
+}
